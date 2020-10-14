83a21332e0cf06713ea572c31c3aeb418a9e4732
Diego Lira <diegoalira@gmail.com>
2020-10-14 11:49:31 -0300

    Style events list

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-    has_many :event_atendees
+    has_many :event_atendees, :dependent => :delete_all
     belongs_to :user, class_name: :User, foreign_key: :creator_id
 end
