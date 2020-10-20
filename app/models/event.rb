9ff99137e9a32ae80ac22f5f59877c5f73710f0d
Diego Lira <diegoalira@gmail.com>
2020-10-20 06:36:05 -0300

    Rubocop offenses

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,8 @@
 class Event < ApplicationRecord
-  
-belongs_to :user, foreign_key: "creator_id"
-has_many :event_atendees
-has_many :attendees, through: :event_atendees, foreign_key: "atendee_id"
+  belongs_to :user, foreign_key: 'creator_id'
+  has_many :event_atendees
+  has_many :attendees, through: :event_atendees, foreign_key: 'atendee_id'
 
-scope :past_events, -> { where('date < ?', Time.now.beginning_of_day) }
-scope :future_events, -> { where('date >= ?', Time.now.beginning_of_day) }
-
+  scope :past_events, -> { where('date < ?', Time.now.beginning_of_day) }
+  scope :future_events, -> { where('date >= ?', Time.now.beginning_of_day) }
 end
--- app/models/event_atendee.rb
+++ app/models/event_atendee.rb
@@ -1,4 +1,4 @@
 class EventAtendee < ApplicationRecord
-  belongs_to :attendees, foreign_key: "atendee_id", class_name: 'User'
-  belongs_to :attended_events, foreign_key: "event_id", class_name: 'Event'
+  belongs_to :attendees, foreign_key: 'atendee_id', class_name: 'User'
+  belongs_to :attended_events, foreign_key: 'event_id', class_name: 'Event'
 end
