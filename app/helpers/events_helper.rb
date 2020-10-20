12c184978fb647e492aedc78e10bff1f8013ee44
Diego Lira <diegoalira@gmail.com>
2020-10-20 06:34:14 -0300

    Remove logic from view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+    def joinable_event(user, event)
+        if user == true && event.date >= Time.now
+            return 1
+        elsif user == "Not registered" && event.date >= Time.now 
+            return 2
+        elsif event.date < Time.now    
+            return 3
+        else 
+            return 4
+        end
+    end    
 end
